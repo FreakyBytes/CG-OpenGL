ef6e40d8d76a1f5bb8a2dc94e821ec53dd2201db
Martin Peters <martin.peters4@uni-rostock.de>
2014-06-30 23:31:51 +0200

    light stuff

--- source/Application/OpenGLWindow.cpp
+++ source/Application/OpenGLWindow.cpp
@@ -9,191 +9,205 @@ using namespace Gui;
 
 namespace Application {
 
-  COpenGLWindow::COpenGLWindow(Terrain::CTerrainModel & terrain)
-    : CPrimaryView(terrain)
-  {
-    mWidth = 1024;
-    mHeight = 768;
-  }
-
-
-  
-  //start up window
-  bool COpenGLWindow::Initialize(int argc, char **argv)
-  {
-    //configure glut
-    CPrimaryView::Initialize(argc, argv);
-    
-    //set clear color
-    glm::vec3 const & clearColor = GetViewStates().GetClearColor();
-    glClearColor(clearColor.r, clearColor.g, clearColor.b, 1.f);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glEnable(GL_NORMALIZE);	//normalize normals when scaling objects
-
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    return GLUtils::CHelper::CheckForError();
-  }
-
-
-  //render loop - will be called every rendered frame
-  void COpenGLWindow::Display(void)
-  {
-    //if window size has changed, recalculate projection matrix
-    if (mProjectionMatrixDirty) {
-      UpdateProjectionMatrix();
-    }
-
-    //get elapsed time since last frame
-    double time = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-
-    //clear screen
-    glm::vec3 const & clearColor = GetViewStates().GetClearColor();
-    glClearColor(clearColor.r, clearColor.g, clearColor.b, 1.f);
-    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-    //clear model view matrix
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    //update scene
-    UpdateScene(time);
-    GLUtils::CHelper::CheckForError();
-
-    //render scene
-    RenderScene();
-    GLUtils::CHelper::CheckForError();
-
-    //show rendered content 
-    ShowRenderedFrame();
-  }
+	COpenGLWindow::COpenGLWindow(Terrain::CTerrainModel & terrain)
+		: CPrimaryView(terrain)
+	{
+		mWidth = 1024;
+		mHeight = 768;
+	}
 
 
 
-  void COpenGLWindow::UpdateScene(double time)
-  {
-    //compute time diff to the last update
-    double timediff = time - GetViewStates().GetLastUpdate();
+	//start up window
+	bool COpenGLWindow::Initialize(int argc, char **argv)
+	{
+		//configure glut
+		CPrimaryView::Initialize(argc, argv);
 
-    //frame counter
-    GetViewStates().IncrementFrame();
+		//set clear color
+		glm::vec3 const & clearColor = GetViewStates().GetClearColor();
+		glClearColor(clearColor.r, clearColor.g, clearColor.b, 1.f);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_CULL_FACE);
+		glEnable(GL_NORMALIZE);	//normalize normals when scaling objects
 
-    //put out fps
-    if (time - GetViewStates().GetFPSTimeBase() > 1.0) {
-      char fpsString[100];
-      sprintf_s(fpsString, "OpenGL Example - FPS: %4.2f - Triangles: %d", GetViewStates().GetFrame() / (time - GetViewStates().GetFPSTimeBase()), GetTerrain().GetNumberOfRenderedTriangles());
-      GetViewStates().SetFPSTimeBase(time);
-      GetViewStates().ResetFrame();
+		// set light 0
+		GLfloat light_pos[] = { 1000.0, 200.0, 1000.0, 0.0 };
+		GLfloat light_direction[] = { 0.0, -1.0, 0.0 };
+		GLfloat light_ambient[] = { 0.8, 0.8, 0.0, 1.0 };
+		GLfloat light_diffuse[] = { 0.8, 0.8, 0.0, 1.0 };
+		GLfloat light_specular[] = { 0.2, 0.2, 0.0, 0.15 };
+		
+		// apply settings for light 0
+		glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light_direction);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 
-      GetViewStates().SetTitelString(fpsString);
-    }
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
 
-    //update camera
-    GetFirstPersonCamera().FrameMove(static_cast<float>(timediff));
-
-    //save time for next frame
-    GetViewStates().SetLastUpdate(time);
-  }
-
-
-
-  void COpenGLWindow::RenderScene(void)
-  {
-    glPolygonMode(GL_FRONT_AND_BACK, GetViewStates().GetWireframeMode() ? GL_LINE : GL_FILL);
-
-    //rotate eye around y-axis
-    const glm::dvec3 eye = GetFirstPersonCamera().GetEyePt();
-    const glm::dvec3 lookAt = GetFirstPersonCamera().GetLookAtPt();
-    const glm::dvec3 upVec = dvec3(0.0f, 0.0f, 1.0f);
-
-    //setup camera
-    gluLookAt(eye.x, eye.y, eye.z,          // eye position in world space
-              lookAt.x, lookAt.y, lookAt.z, // point of interest in world space
-              upVec.x, upVec.y, upVec.z);   // up vector
-
-    //render sky dome
-    RenderSkyDome();
-
-    //render palme
-    //RenderPalm();
-
-    //render terrain
-    RenderTerrain(eye, lookAt, upVec);
-  }
+		return GLUtils::CHelper::CheckForError();
+	}
 
 
+	//render loop - will be called every rendered frame
+	void COpenGLWindow::Display(void)
+	{
+		//if window size has changed, recalculate projection matrix
+		if (mProjectionMatrixDirty) {
+			UpdateProjectionMatrix();
+		}
 
-  void COpenGLWindow::RenderPalm(void)
-  {
-    //save current matrix
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
+		//get elapsed time since last frame
+		double time = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
 
-    //disable culling and enable blending (required for leafs)
-    glDisable(GL_CULL_FACE);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		//clear screen
+		glm::vec3 const & clearColor = GetViewStates().GetClearColor();
+		glClearColor(clearColor.r, clearColor.g, clearColor.b, 1.f);
+		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
+		//clear model view matrix
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
 
-    //transform palm
-    glScalef(100.1f, 100.1f, 70.1f);
-    glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+		//update scene
+		UpdateScene(time);
+		GLUtils::CHelper::CheckForError();
 
-  
-    //render the palm
-    CPrimaryView::RenderPalm();
+		//render scene
+		RenderScene();
+		GLUtils::CHelper::CheckForError();
 
-    //restore states
-    glDisable(GL_BLEND);
-    glEnable(GL_CULL_FACE);
-
-    //restore previous matrix
-    glPopMatrix();
-  }
+		//show rendered content 
+		ShowRenderedFrame();
+	}
 
 
 
-  void COpenGLWindow::RenderTerrain(glm::dvec3 const & eye, glm::dvec3 const & lookAt, glm::dvec3 const & upVec)
-  {
-    //save current matrix
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
+	void COpenGLWindow::UpdateScene(double time)
+	{
+		//compute time diff to the last update
+		double timediff = time - GetViewStates().GetLastUpdate();
 
-    //save current material (being none material)
-    glPushAttrib(GL_LIGHTING_BIT);
+		//frame counter
+		GetViewStates().IncrementFrame();
 
-    //set material definition here 
+		//put out fps
+		if (time - GetViewStates().GetFPSTimeBase() > 1.0) {
+			char fpsString[100];
+			sprintf_s(fpsString, "OpenGL Example - FPS: %4.2f - Triangles: %d", GetViewStates().GetFrame() / (time - GetViewStates().GetFPSTimeBase()), GetTerrain().GetNumberOfRenderedTriangles());
+			GetViewStates().SetFPSTimeBase(time);
+			GetViewStates().ResetFrame();
 
-    //render the palm
-    CPrimaryView::RenderTerrain(eye, lookAt, upVec);
+			GetViewStates().SetTitelString(fpsString);
+		}
 
-    //restore current material
-    glPopAttrib();
+		//update camera
+		GetFirstPersonCamera().FrameMove(static_cast<float>(timediff));
 
-    //restore previous matrix
-    glPopMatrix();
-  }
+		//save time for next frame
+		GetViewStates().SetLastUpdate(time);
+	}
 
 
 
-  //param key: key pressed (only standard keys)
-  //param ctrl: ctrl pressed
-  //param alt: alt pressed
-  bool COpenGLWindow::EvaluateActionKey(unsigned char key, bool ctrl, bool alt)
-  {
-    if (alt) {
-      switch (key) {
-      case 'w':
-        GetViewStates().ToggleWireframeMode();
-        return true;
-      default:
-        return false;
-      }
-    }
-    else {
-      return false;
-    }
-  }
+	void COpenGLWindow::RenderScene(void)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GetViewStates().GetWireframeMode() ? GL_LINE : GL_FILL);
+
+		//rotate eye around y-axis
+		const glm::dvec3 eye = GetFirstPersonCamera().GetEyePt();
+		const glm::dvec3 lookAt = GetFirstPersonCamera().GetLookAtPt();
+		const glm::dvec3 upVec = dvec3(0.0f, 0.0f, 1.0f);
+
+		//setup camera
+		gluLookAt(eye.x, eye.y, eye.z,          // eye position in world space
+			lookAt.x, lookAt.y, lookAt.z, // point of interest in world space
+			upVec.x, upVec.y, upVec.z);   // up vector
+
+		//render sky dome
+		RenderSkyDome();
+
+		//render palme
+		//RenderPalm();
+
+		//render terrain
+		RenderTerrain(eye, lookAt, upVec);
+	}
+
+
+
+	void COpenGLWindow::RenderPalm(void)
+	{
+		//save current matrix
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+
+		//disable culling and enable blending (required for leafs)
+		glDisable(GL_CULL_FACE);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+		//transform palm
+		glScalef(100.1f, 100.1f, 70.1f);
+		glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+
+
+		//render the palm
+		CPrimaryView::RenderPalm();
+
+		//restore states
+		glDisable(GL_BLEND);
+		glEnable(GL_CULL_FACE);
+
+		//restore previous matrix
+		glPopMatrix();
+	}
+
+
+
+	void COpenGLWindow::RenderTerrain(glm::dvec3 const & eye, glm::dvec3 const & lookAt, glm::dvec3 const & upVec)
+	{
+		//save current matrix
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+
+		//save current material (being none material)
+		glPushAttrib(GL_LIGHTING_BIT);
+
+		//set material definition here 
+
+		//render the palm
+		CPrimaryView::RenderTerrain(eye, lookAt, upVec);
+
+		//restore current material
+		glPopAttrib();
+
+		//restore previous matrix
+		glPopMatrix();
+	}
+
+
+
+	//param key: key pressed (only standard keys)
+	//param ctrl: ctrl pressed
+	//param alt: alt pressed
+	bool COpenGLWindow::EvaluateActionKey(unsigned char key, bool ctrl, bool alt)
+	{
+		if (alt) {
+			switch (key) {
+			case 'w':
+				GetViewStates().ToggleWireframeMode();
+				return true;
+			default:
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
 }
